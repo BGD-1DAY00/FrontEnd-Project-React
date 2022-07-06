835c6462546d221774023c65633fdfca41cacd92
Zachary Seeley <zseeley7913@gmail.com>
2022-07-06 15:05:25 -0500

    Created functionality for updating user form

--- src/Admin/CreateComponent.js
+++ src/Admin/CreateComponent.js
@@ -2,37 +2,72 @@ import {useState} from "react";
 import {useDispatch} from "react-redux";
 
 export function CreateComponent() {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [checked, setChecked] = useState(false)
-    console.log(checked)
+
+    const newUser = {
+        username: '',
+        password: '',
+        applicantRole: false,
+        recruiterRole: false,
+        adminRole: false
+    }
+
+    const [formState, setFormState] = useState(newUser)
+
+    // const [username, setUsername] = useState('')
+    // const [password, setPassword] = useState('')
+    // const [checked, setChecked] = useState(false)
+    // console.log(checked)
     //const dispatch = useDispatch();
 
-    function addUsername(e){
-        setUsername(e.target.value);
+    function updateUsername(e){
+        setFormState({
+            ...formState,
+            username: e.target.value
+        })
     }
-    function addPassword(e){
-        setPassword(e.target.value);
+    function updatePassword(e){
+        setFormState({
+            ...formState,
+            password: e.target.value
+        })
+    }
+    function updateApplicantRole() {
+        setFormState({
+            ...formState,
+            applicantRole: !formState.applicantRole
+        })
+    }
+    function updateRecruiterRole() {
+        setFormState({
+            ...formState,
+            recruiterRole: !formState.recruiterRole
+        })
+    }
+    function updateAdminRole() {
+        setFormState({
+            ...formState,
+            adminRole: !formState.adminRole
+        })
     }
     function addUser(e) {
         e.preventDefault()
     }
 
+    console.log(formState)
+
     return (
         <>
             <form onSubmit={addUser}>
                 <label>Username:
-                    <input onChange={addUsername} value={username} placeholder="username" type='text' />
+                    <input onChange={updateUsername} value={formState.username} placeholder="username" type='text' />
                 </label>
-
                 <label>Password:
-                    <input onChange={addPassword} value={password} placeholder="password" type='text' />
+                    <input onChange={updatePassword} value={formState.password} placeholder="password" type='text' />
                 </label>
 
-
-                <input type="checkbox" onChange={() => setChecked(!checked)} checked='applicant' value="applicant" /> Applicant
-                <input type="checkbox" onChange={() => setChecked(!checked)} checked='recruiter' value="recruiter" /> Recruiter
-                <input type="checkbox" onChange={() => setChecked(!checked)} checked='admin' value="admin" />  Admin
+                <input type="checkbox" onChange={updateApplicantRole} checked={formState.applicantRole} /> Applicant
+                <input type="checkbox" onChange={updateRecruiterRole} checked={formState.recruiterRole} /> Recruiter
+                <input type="checkbox" onChange={updateAdminRole} checked={formState.adminRole} />  Admin
 
                 <button  type='submit'>Submit</button>
                 </form>
