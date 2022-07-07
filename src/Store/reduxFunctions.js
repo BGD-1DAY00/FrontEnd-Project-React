3a84fee276f98633a8017890a5ce58675435dffb
Zachary Seeley <zseeley7913@gmail.com>
2022-07-07 10:20:07 -0500

    fixed reduxFunctions.js

--- src/Store/reduxFunctions.js
+++ src/Store/reduxFunctions.js
@@ -1,81 +1,75 @@
-
-import {LOGIN_SUCCESS, LOGIN_REQUEST, LOGIN_FAIL, GET_USERLIST} from "./actions";
-
 import {
-    LOGIN_SUCCESS,
-    LOGIN_REQUEST,
-    LOGIN_FAIL,
-    CREATE_USER,
-    CREATE_USER_SUCCESS,
-    CREATE_USER_FAILED
+	LOGIN_SUCCESS,
+	LOGIN_REQUEST,
+	LOGIN_FAIL,
+	CREATE_USER_SUCCESS,
+	CREATE_USER_FAILED,
+	GET_USERLIST
 } from "./actions";
-import {useSelector} from "react-redux";
 
 
-export function initiateLogin(cred){
-    return async function sideEffect(dispatch){
-        //sending data --> POST request
-        // send it within the params
-        // send it with the body
-        dispatch({type: LOGIN_REQUEST})
+export function initiateLogin(cred) {
+	return async function sideEffect(dispatch) {
+		//sending data --> POST request
+		// send it within the params
+		// send it with the body
+		dispatch({type: LOGIN_REQUEST})
 
-        try{
-            const response = await fetch("http://localhost:8080/login", {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json', // willing to accept
-                    'Content-Type': 'application/json' //defining what we are sending
-                },
-                body: JSON.stringify(cred)
+		try {
+			const response = await fetch("http://localhost:8080/login", {
+				method: 'POST',
+				headers: {
+					'Accept': 'application/json', // willing to accept
+					'Content-Type': 'application/json' //defining what we are sending
+				},
+				body: JSON.stringify(cred)
 
-            })
-            const data = await response.json();
-            console.log(data);
-            dispatch({type: LOGIN_SUCCESS, token: data, role: cred?.role})
-        }catch(e){
-            dispatch({type:LOGIN_FAIL})
-        }
-    }
+			})
+			const data = await response.json();
+			console.log(data);
+			dispatch({type: LOGIN_SUCCESS, token: data, role: cred?.role})
+		} catch (e) {
+			dispatch({type: LOGIN_FAIL})
+		}
+	}
 }
 
-
+export function getUserList() {
+	return async function sideEffect(dispatch) {
+		//sending data --> POST request
+		// send it within the params
+		// send it with the body
+		try {
+			const response = await fetch("http://localhost:8080/getUserList")
+			const data = await response.json();
+			console.log(data);
+			dispatch({type: GET_USERLIST, userList: data})
+		} catch (e) {
+		}
+	}
 }
-
-export function getUserList(){
-    return async function sideEffect(dispatch, getState){
-        //sending data --> POST request
-        // send it within the params
-        // send it with the body
-        try{
-            const response = await fetch("http://localhost:8080/getUserList")
-            const data = await response.json();
-            console.log(data);
-            dispatch({type: GET_USERLIST, userList: data})
-        }catch(e){
-        }
-    }
 
 
 export function createUser(cred) {
-    return async function sideEffect(dispatch) {
-        console.log(cred)
-        console.log(cred.formState)
-        try {
-            const response = await fetch(`http://localhost:8080/createUser?token=${cred.token}`, {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json', // willing to accept
-                    'Content-Type': 'application/json' //defining what we are sending
-                },
-                body: JSON.stringify(cred.formState)
-            })
-            if (response.ok)
-                dispatch({type: CREATE_USER_SUCCESS})
-            else
-                dispatch({type: CREATE_USER_FAILED})
-        } catch (e) {
+	return async function sideEffect(dispatch) {
+		console.log(cred)
+		console.log(cred.formState)
+		try {
+			const response = await fetch(`http://localhost:8080/createUser?token=${cred.token}`, {
+				method: 'POST',
+				headers: {
+					'Accept': 'application/json', // willing to accept
+					'Content-Type': 'application/json' //defining what we are sending
+				},
+				body: JSON.stringify(cred.formState)
+			})
+			if (response.ok)
+				dispatch({type: CREATE_USER_SUCCESS})
+			else
+				dispatch({type: CREATE_USER_FAILED})
+		} catch (e) {
 
-        }
-    }
+		}
+	}
 
 }
