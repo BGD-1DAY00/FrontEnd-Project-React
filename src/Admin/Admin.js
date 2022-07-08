82d1959a29567c726357ff2721a1ad166e68a6b3
saad <osman>
2022-07-08 09:39:43 -0500

    Edit now Works

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -1,9 +1,19 @@
 import {CreateComponent} from "./CreateComponent";
 import {UserList} from "./UserList";
+import {useSelector} from "react-redux";
+import {Impersonate} from "./Impersonate";
 
 export function Admin(){
-
+    const {impersonate} = useSelector(state=>({
+        impersonate: state.admin.impersonate
+    }))
+if(impersonate){
+    return<>
+        <Impersonate />
+    </>
+}
 return <>
+    <h1>Admin page</h1>
     <CreateComponent/>
     <UserList />
 </>
